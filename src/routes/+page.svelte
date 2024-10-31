b5ebdb585a919f21fdf81afef7f7190aa35fa258
JoshuaKellman <joshua@twoals.com>
2024-10-31 17:47:58 -0400

    initial commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,29 @@
 <script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
+    import Potato from './Potato.svelte';
+    import ExplodedPotato from './ExplodedPotato.svelte';
+    
+    let amountOfPotatoes = 14;
+
+    let exploded = Array(amountOfPotatoes).fill(false)
+    
+    console.log(exploded)
+    
+    function handleExplode(number) {
+        exploded[number]=true;
+        console.log("Exploded!");
+    }
+    function handleUnExplode(number) {
+        exploded[number]=false;
+        console.log("unExploded!");
+    }
 </script>
 
-<svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
-</svelte:head>
-
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="grid grid-cols-3">
+{#each exploded as state, i}
+{#if exploded[i]}
+    <a onclick={handleUnExplode(i)}><ExplodedPotato></ExplodedPotato></a>
+{:else}
+    <a onclick={handleExplode(i)}><Potato></Potato></a>
+{/if}
+{/each}
+</div>
---
+++ src/routes/ExplodedPotato.svelte
@@ -0,0 +1,2 @@
+
+<img src="/fries/fries.png">
