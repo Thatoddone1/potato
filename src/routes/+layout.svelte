a32896ef35c7dc144f3be6adcf0d3b6bfbc79b6a
JoshuaKellman <joshua@twoals.com>
2024-11-09 18:40:00 -0500

    added in some hovering to the navbar, this way it doesnt overlap with other elements on screen

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,36 @@
 <script>
 	import '../app.css';
-	import { page } from "$app/stores";
+	import { page } from '$app/stores';
 	let { children } = $props();
 	function url_for(path) {
-    const baseUrl = $page.url.origin;
-    const url = new URL(path, baseUrl);
-    return url.href;
-  }
-  let homeURL = url_for("/");
-  let aboutURL = url_for("/about");
+		const baseUrl = $page.url.origin;
+		const url = new URL(path, baseUrl);
+		return url.href;
+	}
+	let homeURL = url_for('/');
+	let aboutURL = url_for('/about');
+	let showNavBar = $state(false);
+	function mouseHover() {
+		showNavBar = true;
+	}
+	function mouseUnHover() {
+		showNavBar = false;
+	}
 </script>
-<div class="fixed right-0 rounded-xl bg-gradient-to-r from-indigo-600 to-cyan-600 p-3 m-2 z-50">
-<a href={homeURL} class="rounded-xl bg-cyan-400 p-2 m-2">Home</a>
-<a href={aboutURL} class="rounded-xl bg-cyan-400 p-2 m-2">About</a>
+
+/* eslint-disable svelte/a11y */
+<div
+	class="fixed right-0 z-50 m-2 rounded-xl bg-gradient-to-r from-indigo-600 to-cyan-600"
+	class:p-3={showNavBar}
+	class:p-1={!showNavBar}
+	onmouseover={mouseHover}
+	onmouseout={mouseUnHover}
+>
+	{#if showNavBar}
+		<a href={homeURL} class="m-2 rounded-xl bg-cyan-400 p-2">Home</a>
+		<a href={aboutURL} class="m-2 rounded-xl bg-cyan-400 p-2">About</a>
+	{:else}
+		<img src="/potatoes/potato.png" class="size-6" />
+	{/if}
 </div>
 {@render children()}
