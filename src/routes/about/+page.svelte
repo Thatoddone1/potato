3785b6a337c4adc66bf7d1f939665173f4de82a1
JoshuaKellman <joshua@twoals.com>
2024-11-09 22:09:29 -0500

    updated about page. added coming soon text

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,11 @@
 <h1 class="text-center font-mono text-4xl">Potato</h1>
-<p class="text-center text-gray-600">Thank you for visiting potato. Potato was created by <a href="https://joshuarocks.me">thatoddone1</a> as a project expressing his love for potatoes</p>
+<p class="m-3 text-center text-gray-600">
+	Thank you for visiting potato. Potato was created by <a
+		href="https://joshuarocks.me"
+		class="font-bold">thatoddone1</a
+	> as a project expressing his love for potatoes
+</p>
+<hr />
+<p class="m-3 text-center text-gray-600">
+	<bold class="font-bold">coming soon!</bold> more potato content: potato-tac-toe, coming soon to potato.joshuarocks.me
+</p>
