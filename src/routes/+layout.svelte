9bd72fe629f41f0393c7d3b3268f276c5d7434c3
JoshuaKellman <joshua@twoals.com>
2024-11-15 19:03:59 -0500

    added potato-tac-toe game and improved layout styling

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,11 +27,11 @@
 </svelte:head>
 
 <div
-	class="duration-800 group fixed right-1 top-1 m-3 w-16 rounded-3xl bg-blue-400 p-3 transition-all ease-in-out hover:w-80"
+	class="z-50 duration-800 group fixed right-1 top-1 m-3 w-16 rounded-3xl bg-blue-400 p-3 transition-all ease-in-out hover:w-80"
 >
 	<div class="flex gap-3">
 		<!-- svelte-ignore a11y_img_redundant_alt -->
-		<img src="/potatoes/potato.png" alt="a potato" class="size-10 rounded-full" />
+		<img src="/potatoes/potato.png" alt="a potato" class="rounded-full h-10 w-10" />
 		<a
 			class="text-md invisible flex items-center justify-center rounded-md bg-slate-100 p-1 font-semibold group-hover:visible"
 			href={homeURL}
---
+++ src/routes/potato-tac-toe/+page.svelte
@@ -0,0 +1,54 @@
+<script>
+	let potatos = Array(9).fill(0);
+	let currentPlayer = 1;
+
+	function checkWin() {
+		if (potatos[0] == potatos[1] && potatos[1] == potatos[2] && potatos[0] != 0) return true;
+		if (potatos[3] == potatos[4] && potatos[4] == potatos[5] && potatos[3] != 0) return true;
+		if (potatos[6] == potatos[7] && potatos[7] == potatos[8] && potatos[6] != 0) return true;
+		if (potatos[0] == potatos[3] && potatos[3] == potatos[6] && potatos[0] != 0) return true;
+		if (potatos[1] == potatos[4] && potatos[4] == potatos[7] && potatos[1] != 0) return true;
+		if (potatos[2] == potatos[5] && potatos[5] == potatos[8] && potatos[2] != 0) return true;
+		if (potatos[0] == potatos[4] && potatos[4] == potatos[8] && potatos[0] != 0) return true;
+		if (potatos[2] == potatos[4] && potatos[4] == potatos[6] && potatos[2] != 0) return true;
+		return false;
+	}
+
+	function potatoClicked(potatoNumber) {
+		if (currentPlayer == 1) {
+			potatos[potatoNumber] = 1;
+			currentPlayer = 2;
+		} else {
+			potatos[potatoNumber] = 2;
+			currentPlayer = 1;
+		}
+		if (checkWin()) {
+			if (currentPlayer === 1) {
+				alert('Angry Potato wins!');
+			} else {
+				alert('Happy Potato wins!');
+			}
+		}
+	}
+</script>
+
+<button class="fixed left-0 m-2 rounded-xl bg-cyan-300 p-2" onclick={() => location.reload()}>
+	Play again
+</button>
+<div class="p-3 pb-6">
+	<h1 class="text-center font-mono text-3xl">Potato-tac-toe</h1>
+	<p class="text-center font-mono text-sm text-gray-400">tic-tac-toe, but potato</p>
+</div>
+<div class="grid grid-cols-3">
+	{#each potatos as potato, i}
+		{#if potato == 0}
+			<img src="/potatoes/potato.png" onclick={potatoClicked(i)} />
+		{/if}
+		{#if potato == 1}
+			<img src="/potatoes/happy_potato.png" />
+		{/if}
+		{#if potato == 2}
+			<img src="/potatoes/angry_potato.png" />
+		{/if}
+	{/each}
+</div>
