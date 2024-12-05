7bee7f21209991c2b4d6c25cb3ce9e54e3ed1ab1
Joshua Kellman <joshua@twoals.com>
2024-12-05 13:30:37 -0500

    updated about page with additional potato-themed content and refined coming soon message

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,7 +5,10 @@
 		class="font-bold">thatoddone1</a
 	> as a project expressing his love for potatoes
 </p>
+<p class="m-3 text-center text-gray-600">
+	I am a potato, and nobody will ever stop me from being a potato
+</p>
 <hr />
 <p class="m-3 text-center text-gray-600">
-	<bold class="font-bold">coming soon!</bold> more potato content: potato-tac-toe, coming soon to potato.joshuarocks.me
+	<bold class="font-bold">coming soon!</bold> more potato content, coming soon to potato.joshuarocks.me
 </p>
