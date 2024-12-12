bc670c100490e526ee53f9d5ca8b8807c952a6b9
Joshua Kellman <joshua@twoals.com>
2024-12-12 09:26:16 -0500

    added wikipotato(not in navbar yet)

---
+++ src/routes/wikipotato/+page.svelte
@@ -0,0 +1,11 @@
+<style>
+    iframe {
+        width: 100%;
+        height: 100vh;
+        border: none;
+    }
+</style>
+
+<iframe src="https://en.wikipedia.org/wiki/Potato">
+    Wikiepedia Potato
+</iframe>
