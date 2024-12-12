aed231b63647b5954f766ea2a43dd0be5cac6b43
Joshua Kellman <joshua@twoals.com>
2024-12-12 09:43:09 -0500

    Updated potato resources section to use a list format for better organization and readability.

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -16,12 +16,15 @@
 <p class="m-3 text-center text-2xl text-gray-600">
 	<bold class="font-bold">Potato Resources:</bold> 
 </p>
-<p class="m-3 text-center font-bold text-blue-600">
+<ul class="m-3 text-center font-bold text-blue-600">
+	<li>
 	<a
 		href="https://www.potatonewstoday.com"
 		>Potato News Today</a>
+	</li>
+	<li>
 	<a
 		href="https://www.amazon.com/Fresh-Produce-Brands-May-Vary/dp/B07BBLJD8P/ref=sr_1_3_f3_0g_wf"
 		>Order a potato</a>
-	
-</p>
+	</li>
+</ul>
